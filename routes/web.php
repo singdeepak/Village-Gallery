3643cefcdc117e8cd4ef47543b2a666865aa92e6
Deepak Singh <singhdeepak84088@gmail.com>
2025-01-01 10:21:45 +0530

    module/admin-panel

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use App\Http\Controllers\CategoryController;
 
 // Category Resource Route
 Route::resource('category', CategoryController::class);
+
+
+Route::get('show', function(){
+    return view('admin.index');
+});
